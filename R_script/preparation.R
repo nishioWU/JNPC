f616a6c707d1e5b7729f2abaf61325d544820c46
nishioWU <yoshito.nishio+WU@gmail.com>
2018-09-25 16:12:44 +0900

    Rのコード例を実行するのに必要なパッケージのインストール
    ２５日に追加しました

---
+++ R_script/preparation.R
@@ -0,0 +1,12 @@
+# 必要なパッケージのインストール
+# 最初に一度だけ、「Ctrl」 + 「Enter」を繰り返し押して、実行して下さい
+# または、「Source」で一度に実行でも構いません
+
+install.packages("tidyverse")
+install.packages("plotly")
+install.packages("GGally")
+install.packages("scales")
+install.packages("knitr")
+update.packages()
+
+# requireまたはlibraryは、Rを起動するたびに必要です
